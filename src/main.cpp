3e788e032caaa7eb6eda45b8db17c270803c0b41
Martik-k <kosiv.marta@gmail.com>
2025-10-16 20:53:28 +0300

    Added start algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include "motor.h"
 #include "linesensor.h"
+#include "ultrasonic.h"
 
 constexpr int in1Motor1 = 46;
 constexpr int in2Motor1 = 3;
@@ -12,29 +13,31 @@ constexpr int enableMotor2 = 17;
 
 constexpr int linesensorPin = 16;
 
-Motor m1 = Motor(in1Motor1, in2Motor1, enableMotor1);
-Motor m2 = Motor(in1Motor2, in2Motor2, enableMotor2);
 Linesensor l1 = Linesensor(linesensorPin);
 
+constexpr int trig = 11;
+constexpr int echo = 10;
+
+Motor m1 = Motor(in1Motor1, in2Motor1, enableMotor1);
+Motor m2 = Motor(in1Motor2, in2Motor2, enableMotor2);
+Ultrasonic ultrasonic = Ultrasonic(trig, echo);
+
 void setup() {
-    // m1.move(100);
-    // m2.move(100);
-    
-    // delay(5000);
+    Serial.begin(115200);
 
-    // m1.move(-100);
-    // m2.move(-100);
-    
-    // delay(5000);    
-
-    // m1.move(100);
-    // m2.move(-100);
-
-    // m1.stop();
-    // m2.stop();
+    delay(5000);
 }
 
 void loop() {
-    // delay(100);
-    // l1.isLineDetected();
+    long distance = ultrasonic.getDistance();
+    Serial.println(distance);
+
+    if (distance == 150) {
+        m1.move(75);
+        m2.move(-75);
+        return;
+    }
+
+    m1.move(100);
+    m2.move(100);
 }
--- src/ultrasonic.cpp
+++ src/ultrasonic.cpp
@@ -8,12 +8,19 @@ Ultrasonic::Ultrasonic(const uint8_t trig, const uint8_t echo) : trigPin(trig), 
 }
 
 unsigned int Ultrasonic::getDistance() {
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
     digitalWrite(trigPin, HIGH);
     delayMicroseconds(10);
     digitalWrite(trigPin, LOW);
 
-    unsigned int duration = pulseIn(echoPin, HIGH, 30000U);
+    unsigned long duration = pulseIn(echoPin, HIGH, 100000UL);
+
     unsigned int distance = (duration * 0.0343) / 2;
+
+    if (distance == 0) {
+        return 150;
+    }
 
     return distance;
 }
